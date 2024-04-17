02fbbce6bb7ddf1b5383a58b4f8d23a4c05ff882
youngvictormonet <victorstrelnikov91@gmail.com>
2024-04-17 16:33:30 +0300

    initial

---
+++ startup.sh
@@ -0,0 +1,5 @@
+# startup.sh is used by infra/resources.bicep to automate database migrations and isn't used by the sample application
+python manage.py migrate
+gunicorn --workers 2 --threads 4 --timeout 60 --access-logfile \
+    '-' --error-logfile '-' --bind=0.0.0.0:8000 \
+     --chdir=/home/site/wwwroot azureproject.wsgi
