7b66eefdb8532abe6f3fabab1b3ce4ef6ce30754
youngvictormonet <55162493+youngvictormonet@users.noreply.github.com>
2024-04-18 02:28:09 +0300

    Update urls.py

--- azureproject/urls.py
+++ azureproject/urls.py
@@ -15,9 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
-from app import forms, views
+from restaurant_review import forms, views
 
-from app.views import (
+from restaurant_review.views import (
     UsersHTMxTableView
 )
 
