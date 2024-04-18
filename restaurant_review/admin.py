743c06087f6ca3107d84ff4380a40c9cc0c974c3
youngvictormonet <55162493+youngvictormonet@users.noreply.github.com>
2024-04-18 14:22:32 +0300

    Update admin.py

--- restaurant_review/admin.py
+++ restaurant_review/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
-from .models import Restaurant, Review
+from .models import Restaurant, Review, Users
 
 # Register your models here.
 
 admin.site.register(Restaurant)
 admin.site.register(Review)
+admin.site.register(Users)
