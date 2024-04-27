fff312892a3e1f655ce994472e44efe16b99ec30
youngvictormonet <55162493+youngvictormonet@users.noreply.github.com>
2024-04-27 21:08:51 +0300

    Update urls.py

--- restaurant_review/urls.py
+++ restaurant_review/urls.py
@@ -3,7 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.home, name='home'),
+    path("htmx/", UsersHTMxTableView.as_view(), name="users_htmx"),
     path('<int:id>/', views.details, name='details'),
     path('create', views.create_restaurant, name='create_restaurant'),
     path('add', views.add_restaurant, name='add_restaurant'),
