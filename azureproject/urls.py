adc766c0532a19e2b37d6fd4daaf413bbcd506ad
youngvictormonet <55162493+youngvictormonet@users.noreply.github.com>
2024-04-29 19:08:14 +0300

    Update urls.py

--- azureproject/urls.py
+++ azureproject/urls.py
@@ -15,14 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
-from restaurant_review import forms, views
-
-from restaurant_review.views import (
-    UsersHTMxTableView
-)
 
 urlpatterns = [
     path('', include('restaurant_review.urls')),
     path('admin/', admin.site.urls),
-    path("htmx/", UsersHTMxTableView.as_view(), name="users_htmx"),
 ]
