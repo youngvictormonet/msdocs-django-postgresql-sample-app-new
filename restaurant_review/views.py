e0cac200662dc60e3ded4a0147a1485221ad6588
youngvictormonet <55162493+youngvictormonet@users.noreply.github.com>
2024-04-30 22:01:36 +0300

    Update views.py

--- restaurant_review/views.py
+++ restaurant_review/views.py
@@ -58,7 +58,6 @@ def users_uplodad():
 class UsersHTMxTableView(SingleTableMixin, FilterView):
     table_class = UsersHTMxTable
     queryset = Users.objects.all().order_by('id')
-    filterset_class = UsersFilter
     paginate_by = 15
 
     def get_template_names(self):
