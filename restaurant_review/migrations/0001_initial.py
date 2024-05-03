3f49137e23dce4407625f1441d45d24d067563fb
youngvictormonet <55162493+youngvictormonet@users.noreply.github.com>
2024-05-03 14:39:07 +0300

    Update 0001_initial.py

--- restaurant_review/migrations/0001_initial.py
+++ restaurant_review/migrations/0001_initial.py
@@ -47,7 +47,6 @@ class Migration(migrations.Migration):
                 ('tweet_link', models.CharField(max_length=250)),
                 ('wl', models.BooleanField()),
                 ('fcfs', models.BooleanField()),
-                ('invitation_code', models.CharField(max_length=250)),
             ],
         ),
     ]
