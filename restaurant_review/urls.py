44abe69a75a2602cfed1b7cd953ce0ba2ae3dfab
youngvictormonet <55162493+youngvictormonet@users.noreply.github.com>
2024-05-03 21:16:32 +0300

    Update urls.py

--- restaurant_review/urls.py
+++ restaurant_review/urls.py
@@ -7,7 +7,7 @@ from restaurant_review.views import MyView
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('htmx/', UsersHTMxTableView.as_view(), name="users_htmx"),
+    path('htmx', UsersHTMxTableView.as_view(), name="users_htmx"),
     path("about/", MyView.as_view()),
     path('<int:id>/', views.details, name='details'),
     path('create', views.create_restaurant, name='create_restaurant'),
