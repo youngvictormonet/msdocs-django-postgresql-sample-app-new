bd3544b73571d4a80fafd51d0ca876d7f26f3457
youngvictormonet <55162493+youngvictormonet@users.noreply.github.com>
2024-05-03 15:17:30 +0300

    Update views.py

--- restaurant_review/views.py
+++ restaurant_review/views.py
@@ -50,16 +50,15 @@ def users_uplodad():
             'selector': 'th:not(.index_name)',
             'props': 'background-color: white; color: black; font-size: 1em; padding: 0.3em; padding-right: 2em; padding-left: 2em; border-width:5px; border-style: dotted; border-color:black; font-weight: 500; '
         }
-    #all_users = Users.objects.all()
-    #df = pd.DataFrame({'Twitter' : [all_users[id].twitter for id in range(len(all_users)-3,len(all_users))], 'Points': [all_users[id].points for id in range(len(all_users)-3,len(all_users))]}, index = [all_users[id].date_created.strftime("%H:%M:%S")  for id in range(len(all_users)-3,len(all_users))])
-    #table = df.style.set_table_styles([cell_hover, index_names, headers])
-    table = []
+    all_users = Users.objects.all()
+    df = pd.DataFrame({'Twitter' : [all_users[id].twitter for id in range(len(all_users)-3,len(all_users))], 'Points': [all_users[id].points for id in range(len(all_users)-3,len(all_users))]}, index = [all_users[id].date_created.strftime("%H:%M:%S")  for id in range(len(all_users)-3,len(all_users))])
+    table = df.style.set_table_styles([cell_hover, index_names, headers])
     return table
 
 class UsersHTMxTableView(SingleTableMixin, FilterView):
     table_class = UsersHTMxTable
-    #queryset = Users.objects.all().order_by('points')
-    #print(queryset)
+    queryset = Users.objects.all().order_by('points')
+    print(queryset)
     filterset_class = UsersFilter
     paginate_by = 15
     template_name = "restaurant_review/users_table_htmx.html"
