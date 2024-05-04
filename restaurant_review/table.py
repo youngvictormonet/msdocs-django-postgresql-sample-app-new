41f6c6419bc18236e54cbfefaeb787e2cb3b57ac
youngvictormonet <55162493+youngvictormonet@users.noreply.github.com>
2024-05-04 04:07:53 +0300

    Update table.py

--- restaurant_review/table.py
+++ restaurant_review/table.py
@@ -1,8 +1,7 @@
 import django_tables2 as tables
-from restaurant_review.models import Users
+from .models import Users
 
 class UsersHTMxTable(tables.Table):
     class Meta:
         model = Users
         template_name = "restaurant_review/bootstrap_htmx.html"
-
